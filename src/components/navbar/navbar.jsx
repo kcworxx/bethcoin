558437c0fa07eb28f062de8033d0b2f11c8ebff0
kcworxx <kcworxx@gmail.com>
2025-02-14 12:16:01 +0100

    version-1 changes

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './navbar.css';
+import { IoMenu } from "react-icons/io5";
+
+const navbar = () => {
+  return (
+    <div>
+      <div class="nav-content">
+      <div class="logo">
+        <a href="#">BETH.</a>
+      </div>
+      <div>
+        <a href="#">BUY</a>
+      </div>
+      <div>
+      <IoMenu />
+      </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default navbar
